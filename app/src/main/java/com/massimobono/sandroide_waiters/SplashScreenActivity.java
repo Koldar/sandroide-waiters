08094914fe2cd93e67e00aeec0937b8ca47e08e9
DAGGOTH\massi <massimobono1@gmail.com>
2017-03-08 10:00:04 +0100

    ACTIVITIES
    - splash screen updated

--- app/src/main/java/com/massimobono/sandroide_waiters/SplashScreenActivity.java
+++ app/src/main/java/com/massimobono/sandroide_waiters/SplashScreenActivity.java
@@ -1,10 +1,15 @@
 package com.massimobono.sandroide_waiters;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
+/**
+ * This activity handles the loading screen of the application
+ */
 public class SplashScreenActivity extends AppCompatActivity {
 
     private ProgressBar splashScreenProgressBar;
@@ -17,16 +22,61 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         this.splashScreenImage = (ImageView) this.findViewById(R.id.splashScreenImage);
         this.splashScreenProgressBar = (ProgressBar) this.findViewById(R.id.splashScreenProgressBar);
+        this.splashScreenProgressBar.setMax(100);
+        this.splashScreenProgressBar.setProgress(0);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
+        this.loadApplicationData();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+    }
+
+    /**
+     * Loads application data and uses a progress bar to show the loading to the user
+     *
+     * <p>Note: at the moment this functtion loads nothing</p>
+     */
+    private void loadApplicationData() {
+        new AsyncTask<Integer, Integer, Integer>() {
+
+            private int steps = 5;
+
+            @Override
+            protected Integer doInBackground(Integer... params) {
+                for (int i=0; i<steps; i++) {
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                    }
+                    publishProgress((int)((100./steps) * i));
+                }
+                return 0;
+            }
+
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                splashScreenProgressBar.setProgress(values[0]);
+            }
+
+            @Override
+            protected void onPostExecute(Integer integer) {
+                  callMainActivity();
+            }
+        }.execute();
+    }
+
+
+    /**
+     * Create an intent to go to {@link MainActivity}
+     */
+    private void callMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/massimobono/sandroide_waiters/Utils.java
@@ -0,0 +1,9 @@
+package com.massimobono.sandroide_waiters;
+
+/**
+ * The class contain useful methods used through out all the project
+ *
+ * Created by massi on 3/7/2017.
+ */
+public class Utils {
+}
